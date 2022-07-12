3163a3fec2da4190c6242d0f28f2982c3439603d
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-07-12 19:36:06 +0200

    nie kradnij przemek ty kurwo

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -5,54 +5,100 @@ import man from '../../Assets/NavbarIcons/mann.png'
 import grown from '../../Assets/NavbarIcons/rocket.png'
 import code from '../../Assets/NavbarIcons/codee.png'
 import inspire from '../../Assets/NavbarIcons/inspiration.png'
+import { useMediaQuery } from 'react-responsive'
 import './Navbar.scss'
+import BurgerMenu from './BurgerMenu'
 const Navbar = () => {
+    const isPhone = useMediaQuery({
+        query: '(max-width: 700px)',
+    })
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 701px)',
+    })
     return (
         <div className="navbar-container">
-            <div className={'NavBar'}>
-                <div className="n-title">
-                    <img src={logo} className="n-img" alt="default" />
-                    <h2 className="n-title-text">Maciej Wawryszuk</h2>
+            {isPhone && (
+                <div className="navbar-container-desktop">
+                    <BurgerMenu />
                 </div>
-                <ul className="nav">
-                    <li>
-                        <a className="link" to="#">
-                            <img src={man} className="nav-img-class1" />
-                            <div className="circle"></div>
-                            <div className="title">About Me</div>
-                        </a>
-                    </li>
+            )}
+            {isDesktop && (
+                <div className="navbar-container-desktop">
+                    {' '}
+                    <div className="navbar-container-desktop-content">
+                        <img
+                            src={logo}
+                            className="navbar-container-desktop-content-image"
+                            alt="default"
+                        />
+                        <h2 className="navbar-container-desktop-content-title">
+                            Maciej Wawryszuk
+                        </h2>
+                    </div>
+                    <ul className="navbar-container-desktop-table">
+                        <li className="navbar-container-desktop-table-list">
+                            <a
+                                className="navbar-container-desktop-table-list-link"
+                                to="#"
+                            >
+                                <img
+                                    src={man}
+                                    className="navbar-container-desktop-table-list-link-img"
+                                />
+                                <div className="navbar-container-desktop-table-list-link-title">
+                                    About Me
+                                </div>
+                            </a>
+                        </li>
 
-                    <li>
-                        <a className="link" to="#">
-                            <img src={inspire} className="nav-img-class2" />
-                            <div className="circle"></div>
-                            <div className="title">Education</div>
-                        </a>
-                    </li>
+                        <li className="navbar-container-desktop-table-list-link">
+                            <a
+                                className="navbar-container-desktop-table-list-link"
+                                to="#"
+                            >
+                                <img
+                                    src={inspire}
+                                    className="navbar-container-desktop-table-list-link-img"
+                                />
+                                <div className="navbar-container-desktop-table-list-link-title">
+                                    Education
+                                </div>
+                            </a>
+                        </li>
 
-                    <li>
-                        {' '}
-                        <a className="link" to="#">
-                            <img
-                                src={grown}
-                                className="nav-img-class3"
-                                alt="sx"
-                            />
-                            <div className="circle"></div>
-                            <div className="title">Can Do</div>
-                        </a>
-                    </li>
+                        <li className="navbar-container-desktop-table-list-link">
+                            <a
+                                className="navbar-container-desktop-table-list-link"
+                                to="#"
+                            >
+                                <img
+                                    src={grown}
+                                    className="navbar-container-desktop-table-list-link-img"
+                                    alt="sx"
+                                />
+                                <div className="navbar-container-desktop-table-list-link-title">
+                                    Can Do
+                                </div>
+                            </a>
+                        </li>
 
-                    <li>
-                        <a className="link" to="#">
-                            <img src={code} className="nav-img-class4" />
-                            <div className="circle"></div>
-                            <div className="title">Projects</div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                        <li className="navbar-container-desktop-table-list-link">
+                            <a
+                                className="navbar-container-desktop-table-list-link"
+                                to="#"
+                            >
+                                <img
+                                    src={code}
+                                    className="navbar-container-desktop-table-list-link-img"
+                                />
+                                <div className="navbar-container-desktop-table-list-link-title">
+                                    Projects
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
