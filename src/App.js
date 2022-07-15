88873768b483a567c3d67d38884af1b1c610fce1
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-07-15 21:01:24 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/Components/FirstPage/FirstPage.js 	modified:   src/Components/FirstPage/FirstPage.scss 	modified:   src/Components/FivePage/FivePage.js 	modified:   src/Components/FivePage/fivepage.scss 	modified:   src/Components/FourPage/fourpage.scss 	modified:   src/Components/Home.js 	modified:   src/Components/NavBar/Navbar.js 	modified:   src/Components/NavBar/Navbar.scss 	modified:   src/Components/SecondPage/SecondPage.js 	modified:   src/Components/SecondPage/SecondPages.scss 	modified:   src/Components/SixPage/SixPage.js 	modified:   src/Components/SixPage/sixpage.scss 	modified:   src/Components/ThirdPage/ThirdPage.js 	modified:   src/Components/ThirdPage/third.scss 	modified:   src/Components/certyficates/certificates.scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css'
-import FirstPage from './Components/FirstPage/FirstPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home'
-import SecondPage from './Components/SecondPage/SecondPage'
-import ThirdPage from './Components/ThirdPage/ThirdPage'
-import FourPage from './Components/FourPage/FourPage'
-import FivePage from './Components/FivePage/FivePage'
-import SixPage from './Components/SixPage/SixPage'
 
 function App() {
     return (
